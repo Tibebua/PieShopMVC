da820178d9b4822cbd2dbf92bb15d4829e7e995d
Alazar <tibebualazar@gmail.com>
2022-03-22 20:06:33 +0300

    Add project files.

---
+++ PieShopMVC/Controllers/PieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PieShopMVC.Models;
+using PieShopMVC.ViewModels;
+
+namespace PieShopMVC.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PiesListViewModel viewModel = new PiesListViewModel();
+            viewModel.Pies = _pieRepository.AllPies;
+            viewModel.CurrentCategory = "Chess Cake";
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ PieShopMVC/Models/MockCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShopMVC.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category{CategoryId=1, CategoryName="Fruit pies", Description="all fruit pies"},
+                new Category{CategoryId=2, CategoryName="Cheese Cakes", Description="all Cheese Cakes"},
+                new Category{CategoryId=3, CategoryName="Seasonal Pies", Description="all Seasonal Pies"}
+            };
+    }
+}
