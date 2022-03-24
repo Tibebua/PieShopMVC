f6f25a5c3030ce439c4496beeced06e6d300c407
Alazar <tibebualazar@gmail.com>
2022-03-24 16:24:17 +0300

    ef, tag helper, ...

--- PieShopMVC/Controllers/PieController.cs
+++ PieShopMVC/Controllers/PieController.cs
@@ -27,5 +27,14 @@ namespace PieShopMVC.Controllers
 
             return View(viewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+
+            return View(pie);
+        }
     }
 }
---
+++ PieShopMVC/Migrations/20220322171503_notesColumnAdded.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PieShopMVC.Migrations
+{
+    public partial class notesColumnAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "Pies",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "Pies");
+        }
+    }
+}
